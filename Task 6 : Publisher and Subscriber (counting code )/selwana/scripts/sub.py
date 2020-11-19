e35b945dfe0a7103991284e35fb5289330592523
SelwanaShaheen <74108511+SelwanaShaheen@users.noreply.github.com>
2020-11-19 21:05:36 +0200

    Add files via upload

---
+++ Task 6 : Publisher and Subscriber (counting code )/selwana/scripts/sub.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Char
+    
+def callback(data):
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+        
+def listener():
+    
+      
+      rospy.init_node('listener', anonymous=False)
+   
+      rospy.Subscriber("chatter", Char , callback)
+   
+      rospy.spin()
+  
+if __name__ == '__main__':
+      listener()
+
